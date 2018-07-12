6c8b1957b8d0ffb9ec60e635a5c18473b1691b91
lazar030 <lazarjankovic030@email.com>
2018-07-12 18:49:07 +0200

    heroku

--- config/datastores.js
+++ config/datastores.js
@@ -1,6 +1,9 @@
 module.exports.datastores = {
   default: {
-    // adapter: 'sails-mysql',
-    // url: 'mysql://user:password@host:port/database',
-  },
+    heroku_postgres: {
+      adapter: "sails-postgresql",
+      url: process.env.DATABASE_URL,
+      ssl: false
+    }
+  }
 };
